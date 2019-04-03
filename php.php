c3385bd13bc1139cec5af3929aa3e75944e887d1
NullBits9050 <44313802+NullBits9050@users.noreply.github.com>
2019-04-03 10:57:12 +0530

    php

---
+++ php.php
@@ -0,0 +1,21 @@
+<!--"Hello world " script in php
+<html>
+<head>
+  <title> Hello world </title>
+</head>
+<body>
+	<?echo "Hello, World";?>
+</body>
+</html>
+-->
+
+<html>
+<body>
+<?php
+	$array=array(1,2,3,4,5);
+	foreach($array as $value){
+	echo "value is $value<br>"
+}
+?>
+</body>
+</body>
